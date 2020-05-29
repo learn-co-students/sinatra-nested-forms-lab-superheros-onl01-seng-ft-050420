5bd188dd2d7cede421669afa7449aad5c7b3060b
Luke Chen <luke.chen.2010@gmail.com>
2020-05-29 14:54:10 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,15 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
-
+    
+    get '/' do
+      erb :super_hero
+    end
+    
+    post '/teams' do
+      @input = params.to_s
+      
+      erb :team
+    end
 
 end
